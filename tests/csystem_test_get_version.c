0971e8896606c6c81be9fe6a32fc4a8fa961e146
Joachim Metz <joachim.metz@gmail.com>
2016-09-13 08:11:16 +0200

    Worked on Windows test scripts.

--- tests/csystem_test_get_version.c
+++ tests/csystem_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "csystem_test_libcstring.h"
 #include "csystem_test_libcsystem.h"
+#include "csystem_test_macros.h"
 #include "csystem_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int csystem_test_get_version(
 	          LIBCSYSTEM_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	CSYSTEM_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	CSYSTEM_TEST_UNREFERENCED_PARAMETER( argc )
 	CSYSTEM_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( csystem_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	CSYSTEM_TEST_RUN(
+	 "libcsystem_get_version",
+	 csystem_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
